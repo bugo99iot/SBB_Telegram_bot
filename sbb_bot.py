dec0b1791b0fbb959dcb177ededfd27c99ea39b7
Jaspreet <jaspreetsan@gmail.com>
2016-01-28 23:45:36 +0100

    updated

--- sbb_bot.py
+++ sbb_bot.py
@@ -4,7 +4,6 @@ from telegram import Updater
 import requests
 import json
 import datetime
-from telegram.emoji import Emoji
 import telegram
 
 token = '164965900:AAE1_acIuW1lEusHfNrsiGsYVIJWPJOfVcc'
@@ -17,14 +16,21 @@ dispatcher = updater.dispatcher
 
 def start(bot, update):
     print update.message.chat.username
-    bot.sendMessage(chat_id=update.message.chat_id, text="Hello " + update.message.chat.first_name + ", you can check the current train schedule of SBB-CFF with my help, type /help to see the commands, Bon Voyage!")
+    t = """Hello %s, you can check the current train schedule of SBB-CFF with my help,
+    type /help to see the commands, Bon Voyage!""" % update.message.chat.first_name
+    bot.sendMessage(chat_id=update.message.chat_id, text=t)
+
 
 def help(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text="Use /trains <Departure> <Destination> <Via> to get the next 6 trains from your location, note that 'Via' is optional")
+    t = """Use /trains <Departure> <Destination> <Via> to get next 6 trains
+    from your location, note that 'Via' is optional"""
+    bot.sendMessage(chat_id=update.message.chat_id, text=t)
+
 
 def stop(bot, update):
     if update.message.chat.username in admin:
         updater.stop()
+
 
 def echo(bot, update):
     t1 = "Yes, "
@@ -42,10 +48,11 @@ def main():
 
     updater.start_polling()
 
+
 def trains(bot, update, args):
     print args
 
-    for i in range(0,len(args)):
+    for i in range(0, len(args)):
         if "heig" in args[i]:
             args[i] = "yverdon"
 
@@ -54,37 +61,64 @@ def trains(bot, update, args):
     else:
         info = stations(args[0], args[1], args[2])
 
-    bot.sendMessage(chat_id=update.message.chat_id, text=info,  parse_mode= telegram.ParseMode.MARKDOWN)
+    bot.sendMessage(chat_id=update.message.chat_id, text=info)
 
-# dep = raw_input("Enter your departure location:  ")
-# arr = raw_input("Enter your arrival destination:  ")
-# via = raw_input("Enter any station you'd like to pass through: ")
+def hms_to_minutes(t):
+    h, m, s = [int(i) for i in t.split(':')]
+    return 60*h + m
 
-def stations(dep_loc, arr_loc, via = ''):
+def stations(dep_loc, arr_loc, via=''):
+    url_str = "http://transport.opendata.ch/v1/connections?from=%s&to=%s"
+
     if via == '':
-        url_str = "http://transport.opendata.ch/v1/connections?from=%s&to=%s&limit=6" % (dep_loc, arr_loc)
+        url_str = url_str % (dep_loc, arr_loc)
     else:
-        url_str = "http://transport.opendata.ch/v1/connections?from=%s&to=%s&via=%s&limit=6" % (dep_loc, arr_loc, via)
+        url_str = url_str + "&via=%s" % (dep_loc, arr_loc, via)
 
     response = requests.get(url_str)
 
     r = response.content
 
     r = json.loads(r)
+    i = 0
+    res = "Connections from " + r['connections'][i]['from']['location']['name'] + " to " + r['to']['name'] + " \n\n"
 
-    res = " "
-    for i in range(0,len(r['connections'])):
-        timestamp = r['connections'][i]['from']['departureTimestamp']
-        timeDep = datetime.datetime.fromtimestamp(timestamp).strftime('%H:%M')
-        res = res + "Next train from " +r['connections'][i]['from']['location']['name']+ " at *" + timeDep + "* on platform " + r['connections'][i]['from']['platform']
+    nontrains = ["BUS", "M"]
 
-        timestamp = r['connections'][i]['to']['arrivalTimestamp']
-        timeArr = datetime.datetime.fromtimestamp(timestamp).strftime('%H:%M')
-        res = res + " reaching " + r['to']['name']+ " at *" + timeArr + "* on platform " + r['connections'][i]['to']['platform']
-        res = res + "\n\n"
+    for i in range(0, len(r['connections'])):
+
+        for j in range(0, len(r['connections'][i]['sections'])):
+
+            if not r['connections'][i]['sections'][j]['walk']:
+
+                timestamp = r['connections'][i]['sections'][j]['departure']['departureTimestamp']
+                timeDep = datetime.datetime.fromtimestamp(timestamp).strftime('%H:%M')
+
+                timestamp = r['connections'][i]['sections'][j]['arrival']['arrivalTimestamp']
+                timeArr = datetime.datetime.fromtimestamp(timestamp).strftime('%H:%M')
+
+                if r['connections'][i]['sections'][j]['journey']['category'] in nontrains:
+                    res += r['connections'][i]['sections'][j]['departure']['station']['name'] + " [" + timeDep + "] "
+                    res += " --> " + r['connections'][i]['sections'][j]['arrival']['station']['name'] + " [" + timeArr + "] "
+                    res += " [" + r['connections'][i]['sections'][j]['journey']['category'] + "] \n"
+                else:
+                    res += r['connections'][i]['sections'][j]['departure']['station']['name'] + " [" + timeDep + "] (P" + r['connections'][i]['sections'][j]['departure']['platform'] + ")"
+                    res += " --> " + r['connections'][i]['sections'][j]['arrival']['station']['name'] + " [" + timeArr + "] (P" + r['connections'][i]['sections'][j]['arrival']['platform']  + ")"
+                    res += " [" + r['connections'][i]['sections'][j]['journey']['category'] + "] \n"
+
+            else:
+
+                t = r['connections'][i]['sections'][j]['walk']['duration']
+                res += "Walk for %d minutes \n" % hms_to_minutes(t)
+
+        res += "\n"
 
     return res
 
+# Geneva [21:00] (P6) -> Lausanne [21:42] (P2)
+
+
+# print stations("Bergieres", "Geneva")
 
 if __name__ == "__main__":
     main()
